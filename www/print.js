1b2e1221f0c756f0bd311555bdcf5a61a565aa62
Diego Garcia <diego.garcia@pivingenieria.com>
2017-11-17 09:13:30 -0500

    TON Prueba Json and String Receive 3

--- www/print.js
+++ www/print.js
@@ -1,7 +1,7 @@
 module.exports = (function(){
 
     var verificarPlugin = function(args, successCallback, errorCallback){
-        cordova.exec(successCallback, errorCallback, "print", "verificarPluginJava", args);};
+        cordova.exec(successCallback, errorCallback, "print", "verificarPluginJava", [args]);};
 
     var imprimir = function(args,successCallback, errorCallback){
         cordova.exec(successCallback, errorCallback, "print", "printinTerminal", [args]);};
