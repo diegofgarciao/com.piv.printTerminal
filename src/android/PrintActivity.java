ae0b69875e75f817d396f06f73129162e165a490
Diego Garcia <diego.garcia@pivingenieria.com>
2017-11-17 13:51:30 -0500

    TON Prueba Json and String Receive 6

--- src/android/PrintActivity.java
+++ src/android/PrintActivity.java
@@ -106,7 +106,7 @@ private String vacio;
                     print.appendTextEntity2(text);
                     //----------------------------------------------------------------------------
                     ubicationPrint = jsonobject.getString("ubicacion");
-                    if(ubicationPrint == ""){
+                    if(ubicationPrint == out ){
                     callbackContext.success("Valueoff ubicacion");
                     text.text="Valueoff ubicacion";
                     text.isBoldFont=false;
